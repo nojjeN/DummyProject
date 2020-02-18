658509bbc856fbd87a740d9e2206b94a384bda6d
nojjeN <niklas.adam.andersson@gmail.com>
2020-02-18 19:39:00 +0100

    Add bouncing ball

---
+++ Assets/Jump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().AddForce(0, 200, 0);
+    }
+}
